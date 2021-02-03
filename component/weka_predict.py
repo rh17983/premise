93f6cc1bb97931bc72f1aa86d49c415b68a6f5e8
RH <rahim.heydarov@usi.ch>
2021-02-03 15:38:31 +0100

    nintial

---
+++ component/weka_predict.py
@@ -0,0 +1,147 @@
+"""Core module of the premise prediction."""
+import re
+import os
+import json
+from collections import OrderedDict
+import util.localizer_config as localizer_config
+import util.localizer_log as localizer_log
+
+
+if os.path.isfile('weka.json'):
+    with open('weka.json') as f:
+        try:
+            __OPTIONS = json.load(f)
+        except:
+            localizer_log.error('weka.json not in right format.')
+            __OPTIONS = []
+else:
+    localizer_log.error('weka.json not found.')
+__classifiers = OrderedDict(__OPTIONS)
+__predictors = {}
+
+
+def init(arff_path, dst_path):
+    """Main training function of the premise prediction.
+
+    The function uses the WEKA machine learning library, implemented by
+    python-weka-wrapper Python library. It will divide the data into given
+    folds, and do the training and varification. Results and summary will be
+    written to a file.
+
+    Args:
+        arff_path(str): The string that represents the path of the input arff
+            file.
+        dst_path(str): The string that represents the path of the output
+        summary file.
+
+    Returns:
+        None
+    """
+    import weka.core.converters as converters
+    from weka.classifiers import Classifier
+    from weka.classifiers import Evaluation
+    from weka.core.classes import Random
+
+    global __classifiers
+    global __predictors
+
+    data = converters.load_any_file(arff_path)
+    data.class_is_last()
+
+    lines = []
+    summaries = []
+    summary_line = ['Model'.ljust(16),
+                    'Precision'.ljust(12),
+                    'Recall'.ljust(12),
+                    'F-measure'.ljust(12),
+                    'Accuracy'.ljust(12),
+                    'FPR'.ljust(12)]
+    summaries.append('\t'.join(summary_line))
+    for classifier, option_str in __classifiers.items():
+        option_list = re.findall(r'"(?:[^"]+)"|(?:[^ ]+)', option_str)
+        option_list = [s.replace('"', '') for s in option_list]
+
+        classifier_name = classifier.split('.')[-1]
+        info_str = "Using classifier: {classifier}, options: {options}"\
+            .format(classifier=classifier_name,
+                    options=str(option_list))
+        localizer_log.msg(info_str)
+        lines.append(info_str)
+
+        cache = localizer_config.load_model(classifier_name)
+        if cache:
+            cls, data_ = cache
+        else:
+            cls = Classifier(classname=classifier, options=option_list)
+            cls.build_classifier(data)
+            localizer_config.save_model(cls, data, classifier_name)
+
+        __predictors[classifier_name] = cls
+
+        evl = Evaluation(data)
+        evl.crossvalidate_model(cls, data, 10, Random(1))
+
+        print(evl.percent_correct)
+        print(evl.summary())
+        print(evl.class_details())
+
+        lines.append(evl.summary())
+        lines.append(evl.class_details())
+
+        summary_line = []
+        summary_line.append(classifier_name.ljust(16))
+        summary_line.append("{:.3f}".format(evl.weighted_precision *
+                                            100).ljust(12))
+        summary_line.append("{:.3f}".format(evl.weighted_recall *
+                                            100).ljust(12))
+        summary_line.append("{:.3f}".format(evl.weighted_f_measure *
+                                            100).ljust(12))
+        summary_line.append("{:.3f}".format(evl.percent_correct).ljust(12))
+        summary_line.append("{:.3f}".format(evl.weighted_false_positive_rate *
+                                            100)
+                            .ljust(12))
+        summaries.append('\t'.join(summary_line))
+
+    with open(dst_path, 'w') as f:
+        f.writelines('\n'.join(lines))
+        f.writelines('\n'*5)
+        f.writelines('\n'.join(summaries))
+
+
+def pred_seq(exp, arff_path, dst_folder):
+    """The function to generate a detailed prediction sequence of the experiment.
+
+    Args:
+        exp(obj): An util.runtime.Observation object.
+        arff_path(str): The string that represents the path of the input arff
+            file.
+        dst_folder(str): The path of the folder to put the result.
+
+    Returns:
+        None
+    """
+    global __predictors
+    import util.runtime as runtime
+    import weka.core.converters as converters
+    data = converters.load_any_file(arff_path)
+    data.class_is_last()
+    for cls_name, cls in __predictors.items():
+        f_path = os.path.join(dst_folder, cls_name + '.txt')
+        with open(f_path, 'w') as f:
+            lines = []
+            for index, inst in enumerate(data):
+                pred = cls.classify_instance(inst)
+                lines.append(runtime.all_classes[int(pred)])
+            f.writelines('\n'.join(lines))
+
+
+def check_stable_prediction(prediction):
+    """Remains compatibility with ranker code.
+
+    Args
+        prediction(list): A list of True and/or False.
+
+    Returns:
+        None
+    """
+    pass
---
+++ run.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+"""The main module of premise.
+
+Example:
+    $ python3 run.py
+"""
+
+import os
+import weka.core.jvm as jvm
+import util.localizer_log as localizer_log
+import util.localizer_config as localizer_config
+from util.localizer_config import config
+import util.runtime as runtime
+import util.kpi_info as kpi_info
+import component.preprocess as preprocess
+import component.arff_gen as arff_gen
+import component.exp_filter_manager as exp_filter_manager
+import component.weka_predict as weka_predict
+
+
+def run():
+    """Main function of premise.
+
+    Args:
+        None
+
+    Returns:
+        None
+    """
+
+    if config.has_option('default', 'max_heapsize'):
+        jvm.start(config.get('default', 'max_heapsize'))
+    else:
+        jvm.start()
+    # Create the target folder
+    dst_folder = localizer_config.get_folder('dst')
+    localizer_config.reset_path(dst_folder)
+
+    # Load KPI
+    kpi_meta = localizer_config.get_meta_path('kpi')
+    with open(kpi_meta) as f:
+        localizer_log.msg("Reading kpi file...")
+        kpi_info.initialize(f.read())
+    # Write KPI indices
+    kpi_info.write_kpi_indices(
+        localizer_config.get_dst_path('kpi_indices.txt'))
+
+    # Process the original file and put it to
+    if localizer_config.component_enabled('preprocess'):
+        preprocess.preprocess()
+
+    # Read the experiment data
+    localizer_log.msg("Reading experiments...")
+    training_dir = localizer_config.get_src_path('training')
+    runtime.add_all(training_dir)
+    if config.has_option('folder', 'target'):
+        target_dir = localizer_config.get_src_path('target')
+        runtime.add_target(target_dir)
+    localizer_log.msg("Finish Reading.")
+
+    if localizer_config.component_enabled('exp_filter'):
+        exps = exp_filter_manager.filter_(runtime.all_exps)
+    else:
+        exps = runtime.all_exps
+
+    arff_path = localizer_config.get_dst_path('training.arff')
+    arff_gen.gen_file(exps, arff_path)
+
+    if localizer_config.component_enabled('predictor'):
+        localizer_log.msg("Prediction enabled.")
+        localizer_log.msg("Runing prediction")
+        dst_path = localizer_config.get_dst_path('predictions.txt')
+        weka_predict.init(arff_path, dst_path)
+        for exp_id, exp in runtime.targets_exps.items():
+            exp_dst_path = localizer_config.get_dst_path(
+                exp.exp_info['full_name'])
+            localizer_config.reset_path(exp_dst_path)
+            exp_arff_path = os.path.join(exp_dst_path, 'target.arff')
+            arff_gen.gen_file({exp_id: exp}, exp_arff_path, fromzero=True)
+            weka_predict.pred_seq(exp, exp_arff_path, exp_dst_path)
+    else:
+        localizer_log.msg("Prediction not enabled.")
+    jvm.stop()
+
+
+if __name__ == '__main__':
+    run()
